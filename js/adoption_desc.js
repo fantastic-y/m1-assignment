b6be26d2463d4cfd397cba6edc9301ce042730bf
Fan Yang <fantasticy@Fans-MacBook-Pro.local>
2021-07-29 19:15:19 -0700

    add lightbox-toggle but not working

--- js/adoption_desc.js
+++ js/adoption_desc.js
@@ -2,9 +2,12 @@ var photos = [];
 var fileNames = [];
 var imageList = [];
 var image;
+var lightBoxes;
 var text;
 var whole;
 var openList = "<div class='photo'>";
+var openLightbox = '<a href="#/" class="lightbox-toggle">';
+var closeLightbox = "</a>";
 var openCaptionTag = "<p>";
 var closeCaptionTag = "</p>";
 captionTexts = ["armstrong","aurora","bo","cadele","dumbo","guston","rose","shay","turtle","wheaty","wiley","zoro"];
@@ -19,8 +22,9 @@ closeText = "Click Here to Visit More Friends!";
 function album() {
     for (var i=0 ; i<12 ; i++) {
         fileNames.push(captionTexts[i]+"_optimized");
-        photos.push("<img src='images/"+fileNames[i]+".jpg'>");
-        image = openList + photos[i];
+        photos.push("<img src='images/"+fileNames[i]+".jpg' alt=" + captionTexts[i] +">");
+        lightBoxes = openLightbox + photos[i] + closeLightbox
+        image = openList + lightBoxes;
         description = openMiddleTag + openDesc + i +openDescClose + descTexts[i] + divcloseList + divcloseList + divcloseList;
         caption = openCaptionTag + captionTexts[i] + closeCaptionTag;
         whole = divopenList + image + description + caption + divcloseList;
@@ -36,5 +40,29 @@ function displayInfo(animalIndex) {
     document.getElementById("infobox").style.visibility="visible";
 };
 
-
 album();
+
+$(document).ready(function(){
+    /* Open lightbox on button click */
+    $('.lightbox-toggle img').click(function(){
+        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+        $('.box').fadeIn();
+    //Check if lightbox has an image
+        if ($('.box').contents('img')) {
+            $('.box').contents().remove('img'); //If true, clear image
+        }
+    //Get text content in attribute
+        var $altvalue = $(this).attr('alt'); //or var altvalue = $(this).attr('alt');
+        if ($altvalue=="Angkor Wat") {
+            var img = $('#photo:nth-child(1) img').clone(); //Duplicate DOM element
+            $('.box').append(img); //Insert duplicated element in another element
+            }
+    });
+    /* Click to close lightbox */
+    $('.close, .backdrop').click(function(){
+    $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+    $('.backdrop').css('display', 'none');
+    });
+    $('.box').fadeOut();
+    });
+});
